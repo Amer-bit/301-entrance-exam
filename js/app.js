aac5729f94a741c2f4db3a6321fd6004143e538d
Amer-bit <https://github.com/Amer-bit>
2020-03-16 15:57:41 +0200

    almost

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 'use strict'
 
 var sectionDate = document.getElementById('date');
-sectionDate.textContent = new Date().toDateString() ;
+sectionDate.textContent = new Date().toDateString();
 
-var getThningsDone= document.getElementById('getThingsDone');
+var getThningsDone = document.getElementById('getThingsDone');
 var finishingDate = document.getElementById('finishingDate');
 
 
@@ -13,10 +13,10 @@ var resultSection = document.getElementById('results');
 
 var ToDoArrayOfObject = []
 
-function ToDoConstructor(toDo,date){
- this.toDo= toDo ;
- this.date = date;
- ToDoArrayOfObject.push(this);
+function ToDoConstructor(toDo, date) {
+    this.toDo = toDo;
+    this.date = date;
+    ToDoArrayOfObject.push(this);
 
 }
 
@@ -24,49 +24,54 @@ function ToDoConstructor(toDo,date){
 ///render  after submit
 
 var myForm = document.getElementById('form')
-myForm.addEventListener('submit',render)
+myForm.addEventListener('submit', render)
 
-function render(event){
+function render(event) {
     event.preventDefault();
     console.log('he');
     var theGodDamnList = event.target.getThingsDone.value
     var dateOfFinish = event.target.finishingDate.value
 
     // console.log(typeof(theGodDamnList),typeof(dateOfFinish),'dskdjaskldjsakld');
-        
-    var newTodo = new ToDoConstructor(theGodDamnList,dateOfFinish);
+
+    var newTodo = new ToDoConstructor(theGodDamnList, dateOfFinish);
     console.log(ToDoArrayOfObject);
     storeToDoList()
-   
 
-    var ol1 = document.createElement('ol');
-    resultSection.appendChild(ol1);
-    var li1 = document.createElement('li');
-    ol1.appendChild(li1)
-    li1.textContent= `${newTodo.toDo} \n ${newTodo.date}`
-    myForm.reset();
+
+    var ul1 = document.createElement('ul');
+    resultSection.appendChild(ul1);
     
+    var li1 = document.createElement('li');
+    ul1.appendChild(li1)
+    li1.textContent = `${newTodo.toDo} \n ${newTodo.date}`
+    myForm.reset();
+
 }
 
 
 // Stroing data and getting them
 
-function storeToDoList(){
+function storeToDoList() {
 
-  var localToDo =  JSON.stringify(ToDoArrayOfObject)
-  
-  localStorage.setItem('ToDolistKey',localToDo);
+    var localToDo = JSON.stringify(ToDoArrayOfObject)
+
+    localStorage.setItem('ToDolistKey', localToDo);
 }
 
-function getToDoList(){
-   var localToDo = localStorage.getItem('ToDolistKey')
+function getToDoList() {
+    var localToDo = localStorage.getItem('ToDolistKey')
 
-    if(localToDo){
+    if (localToDo) {
 
-        ToDoArrayOfObject =  JSON.parse(localToDo)
+        ToDoArrayOfObject = JSON.parse(localToDo)
     }
-    return render();
-    
-    
+
+
+
 }
- getToDoList()
+getToDoList()
+
+function removeLocal(){
+
+}
