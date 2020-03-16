ce82bd03b89a30c5ff4be1ec1ab8beadb1946a45
Amer-bit <https://github.com/Amer-bit>
2020-03-16 13:20:13 +0200

    finishedQ2

---
+++ q2.js
@@ -0,0 +1,34 @@
+'use strict'
+
+var starsArray = ['*', '**', '***', '****', '*****']
+var i;
+for(i =0 ; i < starsArray.length; i++ ){
+
+    console.log(`${starsArray[i]} \n`);
+    
+
+}
+
+
+// console.log('=========================');
+
+
+// while(true){
+//     var moreStars= prompt('Do You want more stars please answer by yes or no')
+//     moreStars.toLowerCase;
+//     if (moreStars === 'yes'){
+//         var more=  prompt('Enter more stars')
+//     starsArray.push(more)
+//     for(i =0 ; i < starsArray.length; i++ ){
+
+//         console.log(`${starsArray[i]} \n`);
+        
+    
+//     }
+
+// }
+// else{
+//     alert('Ok then')
+//     break;
+// }
+// }
